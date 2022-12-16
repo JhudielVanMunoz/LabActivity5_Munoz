505b0e488b24c4c044de58b0c97e109d802492a1
Jhudiel Muñoz <67956800+jhudiel0101@users.noreply.github.com>
2022-12-16 23:27:04 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,8 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame {
     private JPanel panel1;
@@ -15,6 +17,20 @@ public class FoodOrderGUI extends JFrame {
     private JRadioButton rb5;
     private JRadioButton rb10;
     private JRadioButton rb15;
+    public FoodOrderGUI() {
+        ButtonGroup btnG = new ButtonGroup();
+        btnG.add(rbNone);
+        btnG.add(rb5);
+        btnG.add(rb10);
+        btnG.add(rb15);
+
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calc_order();
+            }
+        });
+    }
 
     public static void main(String[] args) {
         FoodOrderGUI app = new FoodOrderGUI();
@@ -23,5 +39,40 @@ public class FoodOrderGUI extends JFrame {
         app.setSize(600, 700);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
+    }
+
+    public void calc_order() {
+        double total = 0;
+        if (cPizza.isSelected()) {
+            total += 100;
+        }
+        if (cBurger.isSelected()) {
+            total += 80;
+        }
+        if (cFries.isSelected()) {
+            total += 65;
+        }
+        if (cSoftDrinks.isSelected()) {
+            total += 55;
+        }
+        if (cTea.isSelected()) {
+            total += 50;
+        }
+        if (cSundae.isSelected()) {
+            total += 40;
+        }
+
+        double discount = 0;
+        if (rb5.isSelected()) {
+            discount = 0.05;
+        } else if (rb10.isSelected()) {
+            discount = 0.1;
+        } else if (rb15.isSelected()) {
+            discount = 0.15;
+        }
+
+        total -= (total * discount);
+
+        JOptionPane.showMessageDialog(panel1, String.format("The total price is Php %.2f", total));
     }
 }
