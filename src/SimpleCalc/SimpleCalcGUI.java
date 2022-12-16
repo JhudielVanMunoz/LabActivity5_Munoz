14e85623904ab3f443251c3cc760f628746e7d1e
Jhudiel Muñoz <67956800+jhudiel0101@users.noreply.github.com>
2022-12-16 20:11:47 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,8 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
@@ -10,12 +12,45 @@ public class SimpleCalcGUI extends JFrame {
     private JTextField tfNumber2;
     private JTextField lblResult;
 
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                compute();
+            }
+        });
+    }
+
     public static void main(String[] args) {
         SimpleCalcGUI app = new SimpleCalcGUI();
         app.setTitle("Simple Calculator");
         app.setContentPane(app.panel1);
-        app.setSize(500, 700);
+        app.setSize(600, 500);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
+    }
+
+    public void compute() {
+        double first_n = Double.parseDouble(tfNumber1.getText());
+        double second_n = Double.parseDouble(tfNumber2.getText());
+        String op = cbOperations.getSelectedItem().toString();
+
+        double result = 0;
+        switch (op) {
+            case "+":
+                result = first_n + second_n;
+                break;
+            case "-":
+                result = first_n - second_n;
+                break;
+            case "*":
+                result = first_n * second_n;
+                break;
+            case "/":
+                result = first_n / second_n;
+                break;
+        };
+
+        lblResult.setText(Double.toString(result));
     }
 }
